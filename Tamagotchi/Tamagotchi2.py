e3864de7b7e8631c06a9c88f19ede26cf1e71f8f
Claudio Landerer <landerer.claudio@gmail.com>
2023-04-24 14:32:42 +0200

    Refactoring

--- Tamagotchi/Tamagotchi2.py
+++ Tamagotchi/Tamagotchi2.py
@@ -5,38 +5,31 @@ tendenzZurLangeweile = 50
 tendenzZumHunger = 30
 zeitTick = 10
 timeout = 10
-
 name = input("Wie soll dein virtuelles Haustier heißen? ")
 hunger = 5
 langeweile = 5
 muedigkeit = 5
-
 while True: 
     print("")
     print("**************************")
     print("Ein wenig Zeit vergeht ...")
     print("**************************")
-    
     time.sleep(random.randint(zeitTick-zeitTick/2, zeitTick+zeitTick/2))
-    
-    if(random.randint(1,100)<tendenzZumHunger):
+    if random.randint(1,100)<tendenzZumHunger:
         hunger = hunger + 1
-    if(random.randint(1,100)<tendenzZurLangeweile):
+    if random.randint(1,100)<tendenzZurLangeweile:
         langeweile = langeweile + 1
-    if(random.randint(1,100)<tendenzZurMuedigkeit):
+    if random.randint(1,100)<tendenzZurMuedigkeit:
         muedigkeit = muedigkeit + 1
-
     if hunger > 5:
-        print(f"\a____ Ich bin hungrig ... ({hunger}) ____")
+        print("\a____ Ich bin hungrig ... ("+str(hunger)+") ____")
     time.sleep(0.5)
     if langeweile > 5:
-        print(f"\a____ Ich bin gelangweilt ... ({langeweile}) ____")
+        print("\a____ Ich bin gelangweilt ... ("+str(langeweile)+") ____")
     time.sleep(0.5)
     if muedigkeit > 5:
-        print(f"\a____ Ich bin müde ... ({muedigkeit}) ____")
-    
-    aktion = input(f"Was möchtest du mit {name} tun? (1) Spielen (2) Füttern (3) Schlafen lassen (4) nichts? ")
-     
+        print("\a____ Ich bin müde ... ("+str(muedigkeit)+") ____")
+    aktion = input("Was möchtest du mit " +str(name) + " tun? (1) Spielen (2) Füttern (3) Schlafen lassen (4) nichts? ")
     if aktion == "1":
         if langeweile > 3:
             print("____ Super, ich mag spielen ____ ")
@@ -65,9 +58,8 @@ while True:
         print("____")
     else:
         print("____ Ich weiß nicht was du von mir willst ____")
-    
     if hunger > 10 or langeweile > 10 or muedigkeit > 10:
-        break;
+        break
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("Ich mag nicht mehr dein Haustier sein, ich renne jetzt weg ...")
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
