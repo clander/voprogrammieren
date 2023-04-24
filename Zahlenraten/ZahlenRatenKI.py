92a888789492cee11c3f4081831dfae5c0f73d9e
Claudio Landerer <landerer.claudio@gmail.com>
2023-04-24 13:59:36 +0200

    ZahlenRatenKI refactore für BlockPy

--- Zahlenraten/ZahlenRatenKI.py
+++ Zahlenraten/ZahlenRatenKI.py
@@ -3,14 +3,10 @@ def binaersuche_rekursiv(werte, start, ende, anzahlVersuche):
         print('Ich kann deine Zahl nicht gefunden! Ich glaube du schwindelst!')
         return
     mitte = (start + ende) // 2
-    
-    print("Ich rate ... " + str(werte[mitte]))
-    
+    print("Ich rate ... " + str(werte[mitte])) 
     feedback = ""
-   
     while feedback not in ("r","k","g"):
         feedback = input("(r)ichtig (k)leiner oder (g)rößer? ")
-        
     if feedback == "r":
         print("Zahl gefunden. Anzahl versuche: " + str(anzahlVersuche))
         return
@@ -18,13 +14,9 @@ def binaersuche_rekursiv(werte, start, ende, anzahlVersuche):
         return binaersuche_rekursiv(werte, mitte + 1, ende, anzahlVersuche+1)
     elif feedback == "k":
         return binaersuche_rekursiv(werte, start, mitte - 1, anzahlVersuche+1)
-
 def binaersucheKi(werte):
     return binaersuche_rekursiv(werte, 0, len(werte) - 1, 1)
-
-
 min = int(input("Minimum: "))
 max = int(input("Maximum: "))
 moeglicheWerte = list(range(min,max+1))
 binaersucheKi(moeglicheWerte)
-    
