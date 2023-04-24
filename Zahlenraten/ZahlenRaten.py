76982a744e82c5f03a55d4f827f962be3a3a6bd9
Claudio Landerer <landerer.claudio@gmail.com>
2023-04-24 13:47:15 +0200

    Refactoring Zahlenraten  fuer BlockPy

--- Zahlenraten/ZahlenRaten.py
+++ Zahlenraten/ZahlenRaten.py
@@ -2,20 +2,20 @@ import random
 min = 1
 max = 10
 anzahlVersuche = 0
-zufallszahl = random.randint(min,max)
+zufallszahl = random.randint(min, max)
 gefunden = False
-while not gefunden:    
-    eingabe = input(f"Zahl zwischen {min} und {max} eingeben!")
+while not gefunden:
+    eingabe = input("Zahl zwischen" + str(min) + " und " + str(max) + " eingeben!")
     if eingabe.isnumeric():
         eingabeZahl = int(eingabe)
         if eingabeZahl == zufallszahl:
             anzahlVersuche = anzahlVersuche + 1
-            print(f"Du hast die Zahl erraten! Anzahl der Versuche: {anzahlVersuche}")
+            print("Du hast die Zahl erraten! Anzahl der Versuche:" + str(anzahlVersuche))
             gefunden = True
         elif eingabeZahl < zufallszahl:
-            print("Leider nicht, die gesucht Zahl ist größer!")
+            print('Leider nicht, die gesucht Zahl ist größer!')
             anzahlVersuche += 1
         else:
-            print("Leider nicht, die gesucht Zahl ist kleiner!")
+            print('Leider nicht, die gesucht Zahl ist kleiner!')
             anzahlVersuche += 1
-print("Auf Wiedersehen!")
+print('Auf Wiedersehen!')
