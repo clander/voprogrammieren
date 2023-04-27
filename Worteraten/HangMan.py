bcdb0bfc4fa1927d07fb400858f2a44c8b94887c
Claudio Landerer <landerer.claudio@gmail.com>
2023-04-27 14:39:20 +0200

    Hangman verbessert

--- Worteraten/HangMan.py
+++ Worteraten/HangMan.py
@@ -1,18 +1,19 @@
 import random
-wortliste = ["Haus", "Garten", "Zaun", "Mensch", "Dampfschiff", "Katze", "Schule"]
-zufallswort = wortliste[random.randint(0,len(wortliste)-1)]
-ratewort = list("_" * len(zufallswort))
-#print(zufallswort)
+wortliste = ["Haus", "Garten", "Zaun", "Mensch", "Dampfschiff", "Katzenfutter", "Schularbeit"]
+zufallsIndex = random.randint(0,len(wortliste)-1)
+zufallswort = wortliste[zufallsIndex]
+ratewort = list("_"*len(zufallswort))
+print(zufallswort)
 rateversuche = 0
 fehlversuche = 0
 maxfehlversuche = 10
 wortErraten = False
 zuvieleFehlversuche = False
-while(not wortErraten and not zuvieleFehlversuche):
+while not wortErraten and not zuvieleFehlversuche:
     print(ratewort)
-    eingabe = input("Rate einen Buchstaben ... ")
+    eingabe = input("Rate einen Buchstaben ...")
     rateversuche = rateversuche + 1
-    treffer = False 
+    treffer = False
     for index in range(0,len(zufallswort)):
         if ratewort[index] == "_" and eingabe == zufallswort[index]:
             ratewort[index] = zufallswort[index]
@@ -20,8 +21,8 @@ while(not wortErraten and not zuvieleFehlversuche):
     if treffer == False:
         fehlversuche = fehlversuche + 1
         if fehlversuche > maxfehlversuche:
-            print("Du hast leider zuviele Fehlversuche! Spiel beendet!")
+            print("Du hast zuviele Fehlversuche! Spiel leider verloren")
             zuvieleFehlversuche = True
     if ratewort.count("_")==0:
-        print("Super! Du hast das Wort mit " + str(fehlversuche) +" Fehlversuch(en) erraten")
+        print("Gewonnen!")
         wortErraten = True
