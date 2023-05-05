42ab22c3cae5441e17c5c8c5a079d59a67173012
Claudio Landerer <landerer.claudio@gmail.com>
2023-05-05 09:56:24 +0200

    Refactoring

--- Tamagotchi/TamagotchiMicrobit.py
+++ Tamagotchi/TamagotchiMicrobit.py
@@ -1,53 +1,30 @@
-"""
+def on_logo_pressed():
+    global timer
+    timer = 0
+    basic.show_icon(IconNames.HAPPY)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-Z1: ok
+def on_gesture_shake():
+    global timer
+    timer = 0
+    basic.show_icon(IconNames.HAPPY)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-Z2: müde
-
-Z3: hunger
-
-Z4: langeweile
-
-Z5: müde und hungrig
-
-Z6: müde und langweilig
-
-Z7: hungrig und langweilig
-
-Z8: hungrig, langweilig und müde
-
-Jeder Gefühlszustand kann ein oder aus sein (0 oder 1)
-
-Wir haben 3 Stellen
-
-D.h 2 hoch 3 Möglichkeiten
-
-"""
-hunger = 0
-langeweile = 0
-muedigkeit = 0
-zustand = "ok"
+timer = 0
+timer = 0
+basic.show_icon(IconNames.ASLEEP)
 
 def on_every_interval():
-    global muedigkeit, hunger, langeweile
-    muedigkeit += 1
-    hunger += 1
-    langeweile += 1
+    global timer
+    timer += 1
+    if timer == 10:
+        basic.show_icon(IconNames.ASLEEP)
+    elif timer == 20:
+        basic.show_icon(IconNames.SAD)
+    elif timer == 30:
+        basic.show_icon(IconNames.SURPRISED)
+        basic.show_string("z z z")
+    elif timer == 40:
+        basic.show_icon(IconNames.SKULL)
+        control.reset()
 loops.every_interval(1000, on_every_interval)
-
-def on_forever():
-    global zustand
-    if muedigkeit >= 10 or (langeweile >= 10 or hunger >= 10):
-        zustand = "tot"
-    elif muedigkeit <= 5 and (langeweile <= 5 and hunger <= 5):
-        zustand = "ok"
-    else:
-        zustand = ""
-        if muedigkeit > 5:
-            zustand = "" + zustand + "Mue"
-        if langeweile > 5:
-            zustand = "" + zustand + "Lan"
-        if hunger > 5:
-            zustand = "" + zustand + "Hun"
-    basic.show_string(zustand)
-basic.forever(on_forever)
