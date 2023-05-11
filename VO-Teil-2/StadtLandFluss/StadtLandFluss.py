702d6fbb43d053aa12f6d11e0e37b0123e8cd893
Claudio Landerer <landerer.claudio@gmail.com>
2023-05-11 14:39:28 +0200

    Refactoring SLF

--- VO-Teil-2/StadtLandFluss/StadtLandFluss.py
+++ VO-Teil-2/StadtLandFluss/StadtLandFluss.py
@@ -1,27 +1,25 @@
-import random
-import time
+import random, time
 
-anzahlDurchlaeufeEingabe = input("Wieviele Durchläufe möchtest du spielen? ")
-anzahlDurchlaeufe = int(anzahlDurchlaeufeEingabe);
-dauerDurchlaufEingabe = input("Wieviele Sekunden soll ein Durchlauf dauern? ")
-dauerDurchlauf = int(dauerDurchlaufEingabe);
+anzahlDurchlauefe = int(input("Wieviele Durchläufe möchtest du spielen!"))
+dauerDurchlauf = int(input("Wieviele Sekunden soll ein Durchlauf dauern?"))
 
-alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-laenge = len(alphabet)
+alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+laengeAlphabet = len(alphabet)
 
-durchlaufnummer = 1
+durchlauf = 1
 
-while durchlaufnummer <= anzahlDurchlaeufe:
-    zufallszahl = random.randint(0,laenge-1)
-    print("--------------------------------")
-    print("Runde " + str(durchlaufnummer))
-    print("Der Buchstabe von Runde " + str(durchlaufnummer) + " ist: "+ str(alphabet[zufallszahl]))
+while durchlauf <= anzahlDurchlauefe:
+    zufallszahl = random.randint(0, laengeAlphabet-1)
+    print("-----------------------")
+    print("Runde " + str(durchlauf))
+    print("Buchstabe: " + str(alphabet[zufallszahl]))
     vergangeneSekunden = 0
+    print("*"*dauerDurchlauf)
     while vergangeneSekunden < dauerDurchlauf:
         time.sleep(1)
-        t1 = time.time()
-        print('>', flush="True", end='')
+        print('>', flush="True", end="")
         vergangeneSekunden = vergangeneSekunden + 1
-    input("\nDurchlauf für Buchstaben " + alphabet[zufallszahl] + " ist beendet! Weiter mit einer beliebigen Taste ...  ")
-    durchlaufnummer = durchlaufnummer + 1
-print("Danke dass du mit mir gespielt hast. Bis bald!")
+    print("")
+    input("Durchlauf beendet. Nächster Durchlauf mit Enter")
+    durchlauf = durchlauf + 1
+print("Danke für's Spielen!")
