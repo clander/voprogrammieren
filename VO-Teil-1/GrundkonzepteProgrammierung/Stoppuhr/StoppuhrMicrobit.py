6087604002698d26d8f2c339369b083f92545838
Claudio Landerer <landerer.claudio@gmail.com>
2023-05-19 16:06:12 +0200

    Stoppuhr Refactored

--- VO-Teil-1/GrundkonzepteProgrammierung/Stoppuhr/StoppuhrMicrobit.py
+++ VO-Teil-1/GrundkonzepteProgrammierung/Stoppuhr/StoppuhrMicrobit.py
@@ -2,19 +2,14 @@ def on_button_pressed_a():
     global timer_gestartet, sekunden
     timer_gestartet = True
     sekunden = 0
-    basic.show_leds("""
-        . # # # .
-                . # # # .
-                . . # . .
-                . # # # .
-                # # # # #
-    """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global timer_gestartet
-    timer_gestartet = False
-    basic.show_number(sekunden)
+    if timer_gestartet:
+        timer_gestartet = False
+        basic.show_number(sekunden)
+        music.play_melody("F F G A B B B B ", 250)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 sekunden = 0
@@ -26,6 +21,25 @@ def on_every_interval():
     if timer_gestartet:
         sekunden += 1
 loops.every_interval(1000, on_every_interval)
+
+def on_every_interval2():
+    if timer_gestartet:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . # # # .
+                        . . . . .
+                        . . . . .
+        """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        """)
+loops.every_interval(1000, on_every_interval2)
 
 def on_forever():
     pass
